b1555bbc75c8960967dc6fda6e5b4f09fe89bb22
Thiago Brito <thiago83brito@hotmail.com>
2017-02-08 11:59:51 -0200

    Template Url replace by template inside JS file

--- on-off-switch.js
+++ on-off-switch.js
@@ -8,7 +8,13 @@
         active: '='
       },
       restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
-      templateUrl: './on-off-switch.html',
+      template: "<div class=\"onoffswitch\">" +
+        "<input type=\"checkbox\" name=\"onoffswitch\" class=\"onoffswitch-checkbox\" id=\"myonoffswitch\" ng-model=\"active\">"+
+        "<label class=\"onoffswitch-label\" for=\"myonoffswitch\">"+
+          "<span class=\"onoffswitch-inner\"></span>"+
+          "<span class=\"onoffswitch-switch\"></span>"+
+        "</label>"+
+      "</div>",
       replace: true,
       transclude: true,
       link: function($scope, iElm, iAttrs, controller) {
