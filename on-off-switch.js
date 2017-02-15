f656b5c9515ec0064e391c9cebaded17d84bca4b
Thiago Brito <thiago83brito@hotmail.com>
2017-02-15 17:04:12 -0200

    rollback in last changes

--- on-off-switch.js
+++ on-off-switch.js
@@ -5,18 +5,26 @@
   module.directive('onOffSwitch', function(){
     return {
       scope: {
-        active: '=active',
-        onLabel: "@",
-        offLabel: "@"
+        active: '='
+        // ,
+        // onLabel: "@",
+        // offLabel: "@"
       },
       restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
       template: "<div class=\"onoffswitch\">" +
         "<input type=\"checkbox\" name=\"onoffswitch\" class=\"onoffswitch-checkbox\" id=\"myonoffswitch\" ng-model=\"active\">"+
         "<label class=\"onoffswitch-label\" for=\"myonoffswitch\">"+
-          "<span class=\"onoffswitch-inner\">{{onLabel}}</span>"+
-          "<span class=\"onoffswitch-switch\">{{offLabel}}</span>"+
+          "<span class=\"onoffswitch-inner\"></span>"+
+          "<span class=\"onoffswitch-switch\"></span>"+
         "</label>"+
       "</div>",
+      // template: "<div class=\"onoffswitch\">" +
+      //   "<input type=\"checkbox\" name=\"onoffswitch\" class=\"onoffswitch-checkbox\" id=\"myonoffswitch\" ng-model=\"active\">"+
+      //   "<label class=\"onoffswitch-label\" for=\"myonoffswitch\">"+
+      //     "<span class=\"onoffswitch-inner\">{{onLabel}}</span>"+
+      //     "<span class=\"onoffswitch-switch\">{{offLabel}}</span>"+
+      //   "</label>"+
+      // "</div>",
       replace: true,
       transclude: true,
       link: function($scope, iElm, iAttrs, controller) {
@@ -25,7 +33,7 @@
         // })
       },
       controller: function ($scope) {
-        
+
       }
     };
   })
