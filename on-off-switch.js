611f112efde1a71116a838157e58439fd3168a6d
Thiago Brito <thiago83brito@hotmail.com>
2017-02-17 11:37:56 -0200

    Improve in model

--- on-off-switch.js
+++ on-off-switch.js
@@ -12,12 +12,13 @@
       },
       restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
       template: "<div class=\"onoffswitch\">" +
-        "<input type=\"checkbox\" name=\"onoffswitch\" class=\"onoffswitch-checkbox\" id=\"myonoffswitch\" ng-model=\"active\">"+
+        "<input type=\"checkbox\" name=\"onoffswitch\" class=\"onoffswitch-checkbox\" id=\"myonoffswitch\" ng-checked=\"active\">"+
         "<label class=\"onoffswitch-label\" for=\"myonoffswitch\">"+
           "<span class=\"onoffswitch-inner\"></span>"+
           "<span class=\"onoffswitch-switch\"></span>"+
         "</label>"+
       "</div>",
+      // Futher changes
       // template: "<div class=\"onoffswitch\">" +
       //   "<input type=\"checkbox\" name=\"onoffswitch\" class=\"onoffswitch-checkbox\" id=\"myonoffswitch\" ng-model=\"active\">"+
       //   "<label class=\"onoffswitch-label\" for=\"myonoffswitch\">"+
@@ -25,16 +26,6 @@
       //     "<span class=\"onoffswitch-switch\">{{offLabel}}</span>"+
       //   "</label>"+
       // "</div>",
-      replace: true,
-      transclude: true,
-      link: function($scope, iElm, iAttrs, controller) {
-        // $scope.$watch('active', function (value) {
-        //   console.log(value);
-        // })
-      },
-      controller: function ($scope) {
-
-      }
     };
   })
 }).call(this);
